90dc812122bd26499acbc4b8270820382a4fa76b
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-08-25 21:55:02 +0700

    Add code script and obj game
    Add code script and obj game

---
+++ Script/Tomb_data.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tomb_data : MonoBehaviour
+{
+    public Color32 color_tray;
+    public bool[] visible_tray;
+    public int type;
+    public int index_color;
+}
